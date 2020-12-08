4a347471e55226226e7ef3b6214a467d8d06a533
unknown <nathan.c.ginn@gmail.com>
2020-12-08 15:22:25 -0800

    clean old working directory from before deployment attempts

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from './components/Footer'
+import PageHandler from './pages/PageHandler'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <PageHandler />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
