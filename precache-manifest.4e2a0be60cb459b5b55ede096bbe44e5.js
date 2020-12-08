f8ed8c3cea4f13afaa87d69cc6b3c69f5a4ffeb0
unknown <nathan.c.ginn@gmail.com>
2020-12-08 15:38:39 -0800

    Updates

---
+++ precache-manifest.4e2a0be60cb459b5b55ede096bbe44e5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70ab7fc505096a14e9dc3210b7e798c4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2c75124975116edd14ac",
+    "url": "/portfolio/static/css/main.9c859dc9.chunk.css"
+  },
+  {
+    "revision": "e10f72d2d3fa03430d47",
+    "url": "/portfolio/static/js/2.72725981.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portfolio/static/js/2.72725981.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c75124975116edd14ac",
+    "url": "/portfolio/static/js/main.164b92b7.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "93d9f9cd8f00caa9d82a1d0f0e1798b6",
+    "url": "/portfolio/static/media/mosh.93d9f9cd.png"
+  },
+  {
+    "revision": "175dbd0e2a3329111aa7362f52488383",
+    "url": "/portfolio/static/media/trade-jack.175dbd0e.png"
+  },
+  {
+    "revision": "f3275bbfe29a00e67986d2c43064a98a",
+    "url": "/portfolio/static/media/vivere.f3275bbf.png"
+  },
+  {
+    "revision": "f13f6b34fe50d70be48743c7e2dea094",
+    "url": "/portfolio/static/media/weather-app.f13f6b34.png"
+  }
+]);
